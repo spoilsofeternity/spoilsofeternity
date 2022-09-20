3a5cf66b1616375ab64a01c75b71d70a883f2d22
Ben Hoffman <benbrent@gmail.com>
2022-09-20 19:07:53 -0400

    updated docs

--- assets/js/fb378134.811605e3.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkspoils_of_eternity=self.webpackChunkspoils_of_eternity||[]).push([[3100],{4469:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
